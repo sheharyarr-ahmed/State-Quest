9a91b09bb83f9876d70a30ef6fd59592cd55c207
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-01-09 07:37:57 +0500

    created an fake api and learned about the useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
+
+const initialState = {
+  questions: [],
+  //  status can be loading, ready, active, finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      // .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      // .catch((err) => console.log("Error"));
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
