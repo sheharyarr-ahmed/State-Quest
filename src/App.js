080df532959d9beacd3b4ac1bbe5f5d5d3e6e0fb
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-01-09 07:02:20 +0500

    done with teh stup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./Header.js";
+import Main from "./Main.js";
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
