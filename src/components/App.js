1523d8ff38266dea95e52d55c3dd15d25513f85e
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-01-12 11:04:14 +0500

    added the functionality of the finish screen and highscore points

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen.js";
 import Question from "./Question.js";
 import NextButton from "./NextButton.js";
 import Progress from "./Progress.js";
+import FinishScreen from "./FinishScreen.js";
 
 const initialState = {
   //updates that are visible on the screen
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highScore: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -52,16 +54,22 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null,
       };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highScore }, dispatch] =
+    useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
@@ -99,8 +107,20 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status == "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highScore={highScore}
+          />
         )}
       </Main>
     </div>
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,14 @@
+function FinishScreen({ points, maxPossiblePoints, highScore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You Scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)})
+      </p>
+      <p className="highscore">(HighScore: {highScore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
